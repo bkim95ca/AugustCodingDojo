93efeac18ab99427ec46c6e4c5ed7a1cc04454bb
bkim95ca <bkim95ca@gmail.com>
2022-08-16 09:16:44 -0700

    added algos tuesday

---
+++ 00-algos/2-daytwo.js
@@ -0,0 +1,35 @@
+var amazingShow1="Curb your enthusiasm is #1 ! yay :)"; //this is a variable that is equal to a string
+var magicNumber = 23;
+
+amazingShow1 = "The office"; // this is an example of reassigning the variable amazingShow1 to e equal to "the office"
+
+var isRobSpeakingFactsAboutBasketball = false; //booleans are another data type: true or false
+
+//a data type is a simple piece of informatio such as a string, number, boolean
+
+//a data structure is something that can store multiple data types (arrays, objects(hash maps), trees, linked lists)
+
+//an array is a data structure where you can group multiple pieces of information together
+var greatestArtists=["Drake", "J.Cole", "Kendrick", "Kanye", "2Pac", "Beyonce", "Taylor Swift"]; //array of string
+//indices (positions)   0       1
+//actual values you see in the array are called: ELEMENTS
+
+var randomNums = [23, 0, 77, 12, 8]; //array of numbers
+
+var mixedArray = ["Potato", 5, false, true, "hello", 9]; //this is a mixed array- mixed data types
+
+//a loop is used to do something repeatedly
+//for loop increments i before looking at the conditional
+
+// for(var i=0; i<10; i++) { //then increments 3++ 7++, 11++ and 11++ would be our final i
+    // console.log(i); //0, 4, 8
+    // i = i + 3; //then 0+3, 7, 11
+// }
+    
+// console.log("outside of the loop " + i) i is 11++ or 12;
+
+//outputs are 0, 4, 8, outside of the loop 12
+
+
+
+

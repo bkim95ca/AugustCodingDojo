fbcd3c2e272f30795cffa1254c9d3eadc88d6c9a
bkim95ca <bkim95ca@gmail.com>
2022-08-22 08:18:50 -0700

    Pushing fizzbuzz and loop challenge

---
+++ 03-JS/BK-LoopChallenges/loopchallenges.js
@@ -0,0 +1,36 @@
+//challenge 1
+for(let i=1; i<=20; i++) {
+    if(i%2===1) {
+        console.log(i)
+    }
+}
+
+//challenge 2
+for(let i=100; i >= 0; i--) {
+    if(i%3===0) {
+        console.log(i);
+    }
+}
+
+//challenge 3
+for (let i=4; i >= -3.5; i-=1.5) {
+    console.log(i);
+}
+
+//challenge 4
+var sum=0;
+
+for(let i=1; i <= 100; i++) {
+    sum+=i;
+}
+
+console.log(sum);
+
+//challenge 5
+var product = 1;
+
+for(let i=1; i <=12; i++) {
+    product*=i;
+}
+
+console.log(product);

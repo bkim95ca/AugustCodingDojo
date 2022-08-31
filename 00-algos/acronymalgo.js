8ed8ea8bf54eff7eace8f0b2b3a96a7197eb187b
bkim95ca <bkim95ca@gmail.com>
2022-08-31 10:00:15 -0700

    algos

---
+++ 00-algos/acronymalgo.js
@@ -0,0 +1,47 @@
+/* 
+  Acronyms
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+  Do it with .split first if you need to, then try to do it without
+*/
+
+const str1 = "object oriented programming";
+const expectedA = "OOP";
+
+// The 4 pillars of OOP
+const str2 = "abstraction polymorphism inheritance encapsulation";
+const expectedB = "APIE";
+
+const str3 = "software development life cycle";
+const expectedC = "SDLC";
+
+// Bonus: ignore extra spaces
+const str4 = "  global   information tracker    ";
+const expectedD = "GIT";
+
+/**
+ * Turns the given str into an acronym.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} wordsStr A string to be turned into an acronym.
+ * @returns {string} The acronym.
+ */
+function acronymize(str) {
+    emptyString = "";
+
+    if(str[0] != " ") {
+        emptyString += str[0];
+    }
+    for(let i = 0; i < str.length; i++) {
+        if (str[i-1] == " " && str[i] != " ") {
+            emptyString += str[i]
+        }
+    }
+    return emptyString.toUpperCase();
+    //Your code here
+}
+
+console.log(acronymize(str1)); // Expected: "OOP"
+console.log(acronymize(str2)); // Expected: "APIE"
+console.log(acronymize(str3)); // Expected: "SDLC"
+console.log(acronymize(str4)); // Expected: "GIT"
---
+++ 00-algos/algos01python.js
@@ -0,0 +1,38 @@
+/* 
+  String: Reverse
+  Given a string,
+  return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {
+    let newString = ""; //assign blank string for reverse
+    for (let i = str.length-1; i>=0; i--) { //working backwords assigning i as the last letter of the string and decreasing
+      newString += str[i]; //adding the i character of string to the new string
+    }
+    return newString; //returning new string
+}
+
+//TEST CODE FOR REVERSE
+console.log(reverseString(str1)) // Expected: erutaerc
+console.log(reverseString(str2)) // Expected: god
+console.log(reverseString(str3)) // Expected: olleh
+console.log(reverseString(str4)) // Expected: ""t
